b466569277eca918a0797dc11992df05cfacd300
OGSmurfen <chakarov.ilia@gmail.com>
2025-09-05 10:09:39 +0300

    Add project files.

---
+++ MonoGameLibrary/Core.cs
@@ -0,0 +1,114 @@
+﻿using Input;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGameLibrary
+{
+    public class Core : Game
+    {
+        internal static Core s_instance;
+
+        /// <summary>
+        /// Gets a reference to the Core instance.
+        /// </summary>
+        public static Core Instance => s_instance;
+
+        /// <summary>
+        /// Gets the graphics device manager to control the presentation of graphics.
+        /// </summary>
+        public static GraphicsDeviceManager Graphics { get; private set; }
+
+        /// <summary>
+        /// Gets the graphics device used to create graphical resources and perform primitive rendering.
+        /// </summary>
+        public static new GraphicsDevice GraphicsDevice{ get; private set; }
+
+        /// <summary>
+        /// Gets the sprite batch used for all 2D rendering.
+        /// </summary>
+        public static SpriteBatch SpriteBatch { get; private set; }
+
+        /// <summary>
+        /// Gets the content manager used to load global assets.
+        /// </summary>
+        public static new ContentManager Content { get; private set; }
+
+        /// <summary>
+        /// Gets a reference to the input management system.
+        /// </summary>
+        public static InputManager Input { get; private set; } = new InputManager();
+
+        /// <summary>
+        /// Gets or Sets a value that indicates if the game should exit when the esc key on the keyboard is pressed.
+        /// </summary>
+        public static bool ExitOnEscape { get; set; }
+
+        /// <summary>
+        /// Creates a new Core instance.
+        /// </summary>
+        /// <param name="title">The title to display in the title bar of the game window.</param>
+        /// <param name="width">The initial width, in pixels, of the game window.</param>
+        /// <param name="height">The initial height, in pixels, of the game window.</param>
+        /// <param name="fullScreen">Indicates if the game should start in fullscreen mode.</param>
+        public Core(string title, int width, int height, bool fullScreen)
+        {
+            // ensure that multiple cores are not created
+            if (s_instance != null)
+            {
+                throw new InvalidOperationException("Only one instance of Core can be created.");
+            }
+
+            s_instance = this;
+
+            Graphics = new GraphicsDeviceManager(this);
+
+            Graphics.PreferredBackBufferWidth = width;
+            Graphics.PreferredBackBufferHeight = height;
+            Graphics.IsFullScreen = fullScreen;
+            Graphics.ApplyChanges();
+
+            Window.Title = title;
+
+            Content = base.Content;
+
+            Content.RootDirectory = "Content";
+
+            IsMouseVisible = true;
+
+            ExitOnEscape = true;
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            // Set the core's graphics device to a reference of the base Game's
+            // graphics device.
+            GraphicsDevice = base.GraphicsDevice;
+
+            // Create the sprite batch instance.
+            SpriteBatch = new SpriteBatch(GraphicsDevice);
+
+            Input = new InputManager();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            Input.Update(gameTime);
+
+            if(ExitOnEscape && Input.Keyboard.IsKeyDown(Microsoft.Xna.Framework.Input.Keys.Escape))
+            {
+                Exit();
+            }
+
+            base.Update(gameTime);
+        }
+
+    }
+}
---
+++ PlatformerGame/Game1.cs
@@ -0,0 +1,95 @@
+﻿using Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGameLibrary;
+using System;
+
+namespace PlatformerGame
+{
+    public class Game1 : Core
+    {
+
+        private AnimatedSprite _slime;
+        private AnimatedSprite _bat;
+
+        private Vector2 _slimePosition;
+        private const float MOVEMENT_SPEED = 5f;
+
+        public Game1() : base("Platformer Game", 1280, 720, false)
+        {
+
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            // TODO: use this.Content to load your game content here
+            Texture2D atlasTexture = Content.Load<Texture2D>("images/atlas");
+
+            TextureAtlas atlas = TextureAtlas.FromFile(Content, "images/atlas-definition.xml");
+            _slime = atlas.CreateAnimatedSprite("slime-animation");
+            _slime.Scale = new Vector2(4, 4);
+
+            _bat = atlas.CreateAnimatedSprite("bat-animation");
+            _bat.Scale = new Vector2(4, 4);
+
+            base.LoadContent();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            _slime.Update(gameTime);
+            _bat.Update(gameTime);
+
+            CheckKeyboardInput();
+
+            base.Update(gameTime);
+        }
+        private void CheckKeyboardInput()
+        {
+            float speed = MOVEMENT_SPEED;
+            if (Input.Keyboard.IsKeyDown(Keys.Space))
+                speed *= 2f;
+
+            if(Input.Keyboard.IsKeyDown(Keys.W) || Input.Keyboard.IsKeyDown(Keys.Up))
+                _slimePosition.Y -= speed;
+
+            if (Input.Keyboard.IsKeyDown(Keys.A) || Input.Keyboard.IsKeyDown(Keys.Left))
+                _slimePosition.X -= speed;
+
+            if (Input.Keyboard.IsKeyDown(Keys.S) || Input.Keyboard.IsKeyDown(Keys.Down))
+                _slimePosition.Y += speed;
+
+            if (Input.Keyboard.IsKeyDown(Keys.D) || Input.Keyboard.IsKeyDown(Keys.Right))
+                _slimePosition.X += speed;
+
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            SpriteBatch.Begin(samplerState: SamplerState.PointClamp);
+
+            _slime.Draw(SpriteBatch, _slimePosition);
+            _bat.Draw(SpriteBatch, new Vector2(_slime.Width + 10, 0));
+
+            SpriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
